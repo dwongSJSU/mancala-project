c779b7e89d12851cc42fd96df0aeaa0b8e986d52
Akiko0210 <qea643@gmail.com>
2026-04-25 20:43:39 -0700

    binded the model with the view.

--- MancalaTest.java
+++ MancalaTest.java
@@ -5,10 +5,12 @@ import javax.swing.*;
  */
 public class MancalaTest {
     public static void main(String[] args) {
-        MancalaDefaultView m = new MancalaDefaultView();
-        //m.stateChanged(); --> placeholder for now, uncomment for testing to see if stateChanged works()
-        m.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        m.pack();
-        m.setVisible(true);
+        MancalaLinkedList model = new MancalaLinkedList();
+        MancalaDefaultView view = new MancalaDefaultView();
+        view.attachTo(model);
+        view.stateChanged(); // display the real initial board state
+        view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        view.pack();
+        view.setVisible(true);
     }
 }
